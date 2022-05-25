d1441207c9abdbd6d9cd584896a38a5701b19c94
Denis <denispirog878@gmail.com>
2022-05-25 19:45:26 +0300

    Changes

--- Game.cs
+++ Game.cs
@@ -7,7 +7,7 @@ namespace AirHockey
 {
     internal class Game
     {
-        private RenderWindow window = new RenderWindow(new VideoMode(800, 450), "Game window");
+        private RenderWindow window = new RenderWindow(new VideoMode(960, 540), "Game window");
 
         private CircleShape ball;
 
@@ -15,9 +15,6 @@ namespace AirHockey
         private CircleShape player2;
 
         private RectangleShape centerLine;
-
-        private RectangleShape goal1;
-        private RectangleShape goal2;
 
         private double dx = 0.3;
         private double dy = 0.3;
@@ -38,7 +35,6 @@ namespace AirHockey
 
         private void CreateField()
         {
-            //Line
             uint lineWidth = window.Size.X / 100;
             uint lineHeight = window.Size.Y;
 
@@ -50,27 +46,6 @@ namespace AirHockey
                 Position = new SFML.System.Vector2f(lineX, lineY),
                 FillColor = Color.White
             };
-
-            //Goals
-            uint goalWidth = window.Size.X / 100;
-            uint goalHeight = window.Size.Y / 3;
-
-            uint goal1X = 0;
-            uint goal2X = window.Size.X - goalWidth;
-            uint goalY = window.Size.Y / 3;
-            
-
-            goal1 = new RectangleShape(new SFML.System.Vector2f(goalWidth, goalHeight))
-            {
-                Position = new SFML.System.Vector2f(goal1X, goalY),
-                FillColor = Color.Blue
-            };
-
-            goal2 = new RectangleShape(new SFML.System.Vector2f(goalWidth, goalHeight))
-            {
-                Position = new SFML.System.Vector2f(goal2X, goalY),
-                FillColor = Color.Red
-            };                                 
         }
 
         private void CreatePlayers()
@@ -115,7 +90,7 @@ namespace AirHockey
         private void UpdateField()
         {
             ClearField();
-            CalculatePlayer();
+            CalculatePlayers();
             DrawField();
             CalculateBall();
         }
@@ -127,14 +102,16 @@ namespace AirHockey
             window.Clear(gray);
         }
 
-        private void CalculatePlayer()
+        private void CalculatePlayers()
         {
             SFML.System.Vector2i mousePosition = Mouse.GetPosition(window);
 
             if (mousePosition.Y < window.Size.Y - player1.Radius * 2 && mousePosition.Y >= 0)
             {
                 player1.Position = new SFML.System.Vector2f(player1.Position.X, mousePosition.Y);
-            } 
+            }
+
+            player2.Position = new SFML.System.Vector2f(player2.Position.X, ball.Position.Y + 10);
         }
 
         private void CalculateBall()
@@ -142,16 +119,9 @@ namespace AirHockey
             double x = ball.Position.X;
             double y = ball.Position.Y;
             double r = ball.Radius;
-            double x2 = player1.Position.X;
-            double y2 = player1.Position.Y;
 
             x += dx;
             y += dy;
-
-            if(x + r > window.Size.X || (x - r < 0))
-            {
-                dx = -dx;
-            }
 
             if (y + r > window.Size.Y || (y - r < 0))
             {
@@ -160,22 +130,31 @@ namespace AirHockey
 
             double Dx = x - player1.Position.X;
             double Dy = y - player1.Position.Y;
-            double d = Math.Sqrt(Dx * Dx + Dy * Dy); if (d == 0) d = 0.01f;
+            double d = Math.Sqrt(Dx * Dx + Dy * Dy);
+            if (d == 0) d = 0.01f;
 
-            if (d < r + player1.Radius)
+            double Dx1 = x - player2.Position.X;
+            double Dy1 = y - player2.Position.Y;
+            double d1 = Math.Sqrt(Dx1 * Dx1 + Dy1 * Dy1);
+            if (d1 == 0) d1 = 0.01f;
+
+            if (d < r + player1.Radius || d1 < r + player2.Radius)
             {
                 dx = -dx;
                 dy = -dy;
             }
 
+            if (x + r > window.Size.X || (x - r < 0))
+            {
+                x = window.Size.X / 2 - 20;
+                y = window.Size.Y / 2 - 25;
+            }
 
-            ball.Position = new SFML.System.Vector2f((float)x, (float)y);
+            ball.Position = new SFML.System.Vector2f((float)x, (float)y);           
         }
 
         private void DrawField()
         {
-            window.Draw(goal1);
-            window.Draw(goal2);
             window.Draw(player1);
             window.Draw(player2);
             window.Draw(centerLine);
