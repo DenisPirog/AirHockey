8fa8dc363ecff2e0f53c174dd3292c48bccce71e
Denis <denispirog878@gmail.com>
2022-05-27 21:49:50 +0300

    Fix

--- Game.cs
+++ Game.cs
@@ -1,36 +1,68 @@
 ﻿using System;
-using SFML;
+using SFML.System;
 using SFML.Window;
+using System.Threading;
 using SFML.Graphics;
 
 namespace AirHockey
 {
     internal class Game
     {
+        private int width = 960;
+        private int height = 540;
+
         private RenderWindow window = new RenderWindow(new VideoMode(960, 540), "Game window");
 
         private CircleShape ball;
-
-        private CircleShape player1;
-        private CircleShape player2;
-
         private RectangleShape centerLine;
 
-        private double dx = 0.3;
-        private double dy = 0.3;
+        private Player player1;
+        private Player player2;
+
+        private int pointCount = 5;
+
+        private double dx = 6;
+        private double dy = 6;
 
         public void Start()
         {
             window.Closed += WindowClosed;
 
-            CreateField();
-            CreatePlayers();
-            CreateBall();
+            window.SetFramerateLimit(60);
 
-            while (window.IsOpen)
+            CreateField();
+
+            while (!isEndGame())
             {
-                UpdateField();
+                ClearField();
+                CalculatePlayers();
+                CalculateBall();
+                DrawField();
             }
+
+            window.Clear(Color.Cyan);
+            window.Display();
+        }
+
+        private bool isEndGame()
+        {
+            if (player1.score == pointCount)
+            {
+                GameResult(Color.Blue);
+                return true;
+            }
+            else if (player2.score == pointCount)
+            {
+                GameResult(Color.Red);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void GameResult(Color color)
+        {
+            window.Clear(color);
         }
 
         private void CreateField()
@@ -41,41 +73,12 @@ namespace AirHockey
             uint lineX = window.Size.X / 2;
             uint lineY = 0;
 
-            centerLine = new RectangleShape(new SFML.System.Vector2f(lineWidth, lineHeight))
+            centerLine = new RectangleShape(new Vector2f(lineWidth, lineHeight))
             {
-                Position = new SFML.System.Vector2f(lineX, lineY),
+                Position = new Vector2f(lineX, lineY),
                 FillColor = Color.White
             };
-        }
 
-        private void CreatePlayers()
-        {
-            uint playerRadius = 30;
-
-            uint player1X = playerRadius * 2;
-            uint playerY = window.Size.Y / 2 - playerRadius;
-
-            player1 = new CircleShape(playerRadius)
-            {
-                FillColor = Color.Blue,
-                OutlineColor = Color.Black,
-                OutlineThickness = 2,
-                Position = new SFML.System.Vector2f(player1X, playerY)
-            };
-           
-            uint player2X = window.Size.X - playerRadius * 4;
-
-            player2 = new CircleShape(playerRadius)
-            {
-                FillColor = Color.Red,
-                OutlineColor = Color.Black,
-                OutlineThickness = 2,
-                Position = new SFML.System.Vector2f(player2X, playerY),
-            };           
-        }
-
-        private void CreateBall()
-        {
             Color gray = new Color(58, 58, 58);
 
             ball = new CircleShape(25)
@@ -83,16 +86,11 @@ namespace AirHockey
                 FillColor = gray,
                 OutlineColor = Color.Black,
                 OutlineThickness = 3,
-                Position = new SFML.System.Vector2f(window.Size.X / 2 - 20, window.Size.Y / 2 - 25),
+                Position = new Vector2f(window.Size.X / 2 - 20, window.Size.Y / 2 - 25),
             };
-        }
 
-        private void UpdateField()
-        {
-            ClearField();
-            CalculatePlayers();
-            DrawField();
-            CalculateBall();
+            player1 = new Player(Color.Blue, 30, window, 1);
+            player2 = new Player(Color.Red, 30, window, 2);
         }
 
         private void ClearField()
@@ -104,59 +102,87 @@ namespace AirHockey
 
         private void CalculatePlayers()
         {
-            SFML.System.Vector2i mousePosition = Mouse.GetPosition(window);
+            float newPlayerX = player1.mesh.Position.Y + Input();
 
-            if (mousePosition.Y < window.Size.Y - player1.Radius * 2 && mousePosition.Y >= 0)
+            if (newPlayerX <= height - player1.mesh.Radius * 2 && newPlayerX >= 0)
             {
-                player1.Position = new SFML.System.Vector2f(player1.Position.X, mousePosition.Y);
+                player1.mesh.Position = new Vector2f(player1.mesh.Position.X, newPlayerX);
+            }
+            
+            player2.mesh.Position = new Vector2f(player2.mesh.Position.X, ball.Position.Y);
+        }
+
+        private int Input()
+        {
+            int speed = 9;
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            {
+                return -speed;
+            }
+            else if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            {
+                return speed;
             }
 
-            player2.Position = new SFML.System.Vector2f(player2.Position.X, ball.Position.Y + 10);
+            return 0;
         }
 
         private void CalculateBall()
         {
+            double r = ball.Radius;
             double x = ball.Position.X;
             double y = ball.Position.Y;
-            double r = ball.Radius;
 
             x += dx;
             y += dy;
 
-            if (y + r > window.Size.Y || (y - r < 0))
+            if (y + r > window.Size.Y - ball.Radius || y - r < 0 - ball.Radius)
             {
                 dy = -dy;
             }
 
-            double Dx = x - player1.Position.X;
-            double Dy = y - player1.Position.Y;
-            double d = Math.Sqrt(Dx * Dx + Dy * Dy);
-            if (d == 0) d = 0.01f;
+            double distance1 = CalculateDistance(x, y, player1);
+            double distance2 = CalculateDistance(x, y, player2);
 
-            double Dx1 = x - player2.Position.X;
-            double Dy1 = y - player2.Position.Y;
-            double d1 = Math.Sqrt(Dx1 * Dx1 + Dy1 * Dy1);
-            if (d1 == 0) d1 = 0.01f;
-
-            if (d < r + player1.Radius || d1 < r + player2.Radius)
+            if (distance1 < r + player1.mesh.Radius || distance2 < r + player2.mesh.Radius)
             {
                 dx = -dx;
                 dy = -dy;
             }
 
-            if (x + r > window.Size.X || (x - r < 0))
+            if (x + r > window.Size.X - ball.Radius || x - r < 0 - ball.Radius)
             {
-                x = window.Size.X / 2 - 20;
-                y = window.Size.Y / 2 - 25;
+                dx = -dx;
             }
 
-            ball.Position = new SFML.System.Vector2f((float)x, (float)y);           
+            ball.Position = new Vector2f((float)x, (float)y);           
+        }
+
+        private void RespawnBall()
+        {
+            ball.Position = new Vector2f(window.Size.X / 2 - 20, window.Size.Y / 2 - 25);
+        }
+
+        private double CalculateDistance(double x, double y, Player player)
+        {
+            double Dx = x - player.mesh.Position.X;
+            double Dy = y - player.mesh.Position.Y;
+
+            double distance = Math.Sqrt(Dx * Dx + Dy * Dy);
+
+            if (distance == 0)
+            {
+                distance = 0.01f;
+            }
+
+            return distance;
         }
 
         private void DrawField()
         {
-            window.Draw(player1);
-            window.Draw(player2);
+            window.Draw(player1.mesh);
+            window.Draw(player2.mesh);
             window.Draw(centerLine);
             window.Draw(ball);
             window.Display();
---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace AirHockey
+{
+    public class Player
+    {
+        public CircleShape mesh = new CircleShape();
+        public int score = 0;
+        public bool isBot = false;
+
+        public Player(Color meshColor, uint radius, RenderWindow window, int a)
+        {
+            mesh.FillColor = meshColor;
+            mesh.Radius = radius;
+            mesh.OutlineColor = Color.Black;
+            mesh.OutlineThickness = 2;
+
+            uint player1X = radius * 2;
+            uint player2X = window.Size.X - player1X;
+            uint playerY = window.Size.Y / 2 - radius;
+
+            if(a == 1) mesh.Position = new Vector2f(player1X, playerY);
+            if(a == 2) mesh.Position = new Vector2f(player2X, playerY);
+        }
+    }
+}
